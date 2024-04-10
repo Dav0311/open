82c3e98c3121a6dada0c82509153fd8cde935b6e
dave <dave>
2024-04-10 11:31:01 +0300

    project web

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    //get all student data
+
+    //create student 
+
+    //update student info
+
+    //delete student 
+    
+}
